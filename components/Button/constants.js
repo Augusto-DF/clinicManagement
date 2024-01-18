012dbced17c5e9e3c0ad19043c165c56fdf2c09c
Augusto-DF <neoaugustocode@gmail.com>
2024-01-18 12:11:21 -0300

    feat: Create button component

---
+++ components/Button/constants.js
@@ -0,0 +1,5 @@
+export const BUTTON_THEMES = {
+  DEFAULT: "default",
+  OUTLINED: "outlined",
+  TERTIARY: "tertiary",
+};
---
+++ components/Button/index.jsx
@@ -0,0 +1,29 @@
+"use client";
+import React from "react";
+import { BUTTON_THEMES } from "./constants";
+import styles from "./styles.module.css";
+
+const Button = ({
+  children,
+  type = "button",
+  onClick,
+  theme = BUTTON_THEMES.DEFAULT,
+  ...props
+}) => {
+  const handleClick = () => {
+    onClick();
+  };
+
+  return (
+    <button
+      type={type}
+      className={`${styles.button} ${styles[theme]}`}
+      onClick={handleClick}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
