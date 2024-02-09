9437a387b51bfadf033bc64c1de575582eeea61a
Augusto-DF <neoaugustocode@gmail.com>
2024-02-09 10:10:13 -0300

    feat: Create employee model

---
+++ app/api/models/Employee/employee.js
@@ -0,0 +1,24 @@
+class Employee {
+  constructor(userId, specialty) {
+    this.userId = userId;
+    this.specialty = specialty;
+  }
+
+  static createEmployeeTable() {
+    return `
+        CREATE TABLE IF NOT EXISTS employee (
+            id INTEGER PRIMARY KEY AUTOINCRMENT,
+            userId INTEGER NOT NULL,
+            specialty TEXT NOT NULL,
+            CONSTRAINT fk_users
+                FOREIGN KEY (userId)
+                REFERENCES users(id)
+        )
+    `;
+  }
+
+  getEmployee() {
+    const { ...employee } = this;
+    return employee;
+  }
+}
