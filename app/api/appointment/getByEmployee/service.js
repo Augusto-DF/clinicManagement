c7b2c539bd008ff31f757e2698a3c3435170e72e
Augusto-DF <neoaugustocode@gmail.com>
2024-02-13 11:07:30 -0300

    feat: Add updateAppointment service

--- app/api/appointment/getByEmployee/service.js
+++ app/api/appointment/getByEmployee/service.js
@@ -1,7 +1,27 @@
 import { db } from "../../api-configs";
 import { dbResponse } from "../../utils";
 
+export const updateAppointment = async (data) => {
+  let valuesToChange = "";
+
+  valuesToChange = Object.keys(data)
+    .map((key) => {
+      if (key !== "id") return `${key}=@${key}`;
+    })
+    .filter((element) => !!element)
+    .join(", ");
+
+  const query = `
+    UPDATE appointments SET ${valuesToChange} WHERE id=@id
+  `;
+
+  const res = db.prepare(query).run(data);
+
+  return dbResponse(!!res, { message: "Appointment updated" });
+};
+
 export const getAppointmentsByEmployee = async (data) => {
+  updateAppointment({ id: 4, description: "test", employeeId: 4 });
   const query = `
     SELECT * FROM appointments 
         WHERE employeeId=@employeeId AND 
